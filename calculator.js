8ce32ff1639ba452a167b6bb74a5cb59fbfcafe2
therevealer <officialnfluence@gmail.com>
2022-08-11 22:47:28 +0100

    Selected the html elements

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('#number-key');
+const operatorButtons = document.querySelectorAll('#operator-key');
+const allClearKey = document.querySelector('#all-clear-key');
+const deleteKey = document.querySelector('#delete-key');
+const equalsKey = document.querySelector('#equals-key');
+const previousOperation = document.querySelector('#previous-operand');
+const currentOperation = document.querySelector('#current-operand');
+
